84e32b722fd8131394e196c388e3615e6cc9e0ec
hamidreza <takloohamidreza@gmail.com>
2022-11-10 14:25:11 -0500

    feat: Config routese for Home and Blogs components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./container/Home";
+import Blogs from "./container/Blogs";
 function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Blogs />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
