dd006738ff9fcccbda910584292aa4463aa7e96e
hamidreza <takloohamidreza@gmail.com>
2022-11-10 14:28:31 -0500

    Feat: Wrap all components in a hoc component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./container/Home";
 import Blogs from "./container/Blogs";
+import Layout from "./hoc/Layout";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<Blogs />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Blogs />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 }
 
