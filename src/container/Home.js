b8f4e00670c160bd5f17adc9de5110182c016e4b
hamidreza <takloohamidreza@gmail.com>
2022-11-11 06:28:27 -0500

    Fix: Add Home container html elements
    Home container includes:
    1-title (h1)
    2-description (p)
    3- two links to other routes

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,5 +1,27 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>
+            Read and Write articles
+            <span> Increase your knowledge </span>
+          </h1>
+
+          <p>
+            Discover stories, thinking, and expertise from writers on any topic
+            start reading or write an article now
+          </p>
+
+          <div>
+            <a href="/get-started">Get Started</a>
+
+            <a href="/about">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
