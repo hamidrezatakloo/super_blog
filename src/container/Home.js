2eb5785b23ed7deeac622fde64908be9e7cfbebf
hamidreza <takloohamidreza@gmail.com>
2022-11-11 06:43:01 -0500

    Fix: Set fontSize and padding for >640px screens

--- src/container/Home.js
+++ src/container/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="bg-gray-900 text-white flex-1">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
-          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent">
+          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl sm:py-2">
             Read and Write articles
             <span> Increase your knowledge </span>
           </h1>
