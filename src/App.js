57bf690984f6e9d900d6370644cf6b72734a8ab6
hamidreza <takloohamidreza@gmail.com>
2022-11-11 09:14:08 -0500

    Style: Rename Blogs to Blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./container/Home";
-import Blogs from "./container/Blogs";
+import Blog from "./container/Blog";
 import Layout from "./hoc/Layout";
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/" element={<Blogs />}></Route>
+          <Route path="/" element={<Blog />}></Route>
         </Routes>
       </BrowserRouter>
     </Layout>
