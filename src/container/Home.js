7362b31886700ea4ebaaa484f2c56c753d273c6f
hamidreza <takloohamidreza@gmail.com>
2022-11-11 06:45:29 -0500

    Fix: Set display Block for >640px screens

--- src/container/Home.js
+++ src/container/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl sm:py-2">
             Read and Write articles
-            <span> Increase your knowledge </span>
+            <span className="sm:block"> Increase your knowledge </span>
           </h1>
 
           <p>
