6eb1d1d14af5676c7d10abb940fe763ec41ca210
hamidreza <takloohamidreza@gmail.com>
2022-11-15 03:42:02 -0500

    Fix: Add a top post at top of blog tab
    note: this post later will load from backend

--- src/container/Blog.js
+++ src/container/Blog.js
@@ -1,5 +1,20 @@
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section>
+      <div>
+        <h1>Hot Article</h1>
+        <div>
+          <img src="./webpack.png" alt="hotArticle" />
+          <h2>Why we need webpack in frontend development</h2>
+          <p>
+            Webpack is a tool that lets you compile JavaScript modules. It is
+            also known as a module bundler. Given a large number of files, it
+            generates a single file (or a few files) that run your app.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
