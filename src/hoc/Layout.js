f03aba290e2ede9b181798fcb9829c27d6e559d9
hamidreza <takloohamidreza@gmail.com>
2022-11-16 16:00:50 -0500

    Fix: Remove navbar when route is signUp or login

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,9 +1,17 @@
 import Navbar from "../components/Navbar";
-
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 const Layout = ({ children }) => {
+  const [showNav, setShowNav] = useState(true);
+  let location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/login" || location.pathname === "/signup")
+      setShowNav(false);
+    else setShowNav(true);
+  }, [location]);
   return (
     <div className="flex flex-col h-screen">
-      <Navbar />
+      {showNav && <Navbar />}
       {children}
     </div>
   );
