383296e056395aaa9d506d772b9a4c07a38c3f1b
hamidreza <takloohamidreza@gmail.com>
2022-11-16 15:59:35 -0500

    Fix: Add login and signUp to react-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./container/Home";
 import Blog from "./container/Blog";
 import Layout from "./hoc/Layout";
+import Signup from "./container/Signup";
+import Login from "./container/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
