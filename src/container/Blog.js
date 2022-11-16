39033b5fc3cbe909a40d695e38b909a867266b38
hamidreza <takloohamidreza@gmail.com>
2022-11-16 14:01:40 -0500

    Fix: Wrap Home post in a div with 12 col grid

--- src/container/Blog.js
+++ src/container/Blog.js
@@ -3,20 +3,22 @@ const Blog = () => {
     <section className="p-2 sm:p-8">
       <div className="border-y-2 border-black max-w-screen-xl 2xl:max-w-screen-2xl mx-auto pb-[50px]">
         <h1 className="font-bold text-2xl my-4">Hot Article</h1>
-        <div className=" flex justify-center flex-col">
-          <img
-            src="./webpack.png"
-            alt="hotArticle"
-            className="rounded object-contain"
-          />
-          <h2 className="font-medium text-2xl my-4">
-            Why we need webpack in frontend development
-          </h2>
-          <p className="text-gray-500 font-medium">
-            Webpack is a tool that lets you compile JavaScript modules. It is
-            also known as a module bundler. Given a large number of files, it
-            generates a single file (or a few files) that run your app.
-          </p>
+        <div className="grid grid-cols-12">
+          <div className=" flex justify-center flex-col">
+            <img
+              src="./webpack.png"
+              alt="hotArticle"
+              className="rounded object-contain"
+            />
+            <h2 className="font-medium text-2xl my-4">
+              Why we need webpack in frontend development
+            </h2>
+            <p className="text-gray-500 font-medium">
+              Webpack is a tool that lets you compile JavaScript modules. It is
+              also known as a module bundler. Given a large number of files, it
+              generates a single file (or a few files) that run your app.
+            </p>
+          </div>
         </div>
       </div>
     </section>
