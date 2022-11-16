78aaf35f474cce295578251dd5dbacdaf7874a34
hamidreza <takloohamidreza@gmail.com>
2022-11-16 15:51:06 -0500

    Fix: Replace a tag with Link and correct paths

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header aria-label="Site Header" className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div>
-            <a href="/" className="block text-teal-600">
+            <Link to="/" className="block text-teal-600">
               <svg
                 className="h-8"
                 viewBox="0 0 28 24"
@@ -16,22 +17,22 @@ const Navbar = () => {
                   fill="currentColor"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
             <nav aria-label="Site Nav" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
                 <li className="text-gray-500 transition hover:text-gray-500/75">
-                  <a href="/">About</a>
+                  <Link to="/about">About</Link>
                 </li>
 
                 <li className="text-gray-500 transition hover:text-gray-500/75">
-                  <a href="/">Bookmarks</a>
+                  <Link to="/bookmark">Bookmarks</Link>
                 </li>
 
                 <li className="text-gray-500 transition hover:text-gray-500/75">
-                  <a href="/">Blogs</a>
+                  <Link to="/blog">Blog</Link>
                 </li>
               </ul>
             </nav>
@@ -39,26 +40,27 @@ const Navbar = () => {
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
-              <a
-                href="/"
+              <Link
+                to="/login"
                 className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
               >
                 Login
-              </a>
+              </Link>
 
               <div className="hidden sm:flex">
-                <a
-                  href="/"
+                <Link
+                  to="/signup"
                   className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
                 >
                   Register
-                </a>
+                </Link>
               </div>
             </div>
 
             <div className="block md:hidden">
               <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
                 <svg
+                  className="h-5 w-5"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
