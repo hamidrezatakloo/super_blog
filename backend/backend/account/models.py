a04b07afe43c0d6f2ead7d801768cb07e2267ea8
hamidreza <takloohamidreza@gmail.com>
2022-11-18 15:34:13 -0500

    Feat: Create custom user model
    in this custom user model email is use as primary instead of
    username

--- backend/backend/account/models.py
+++ backend/backend/account/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,name,password,**args):
+        if email is None:
+            raise ValueError('No email Provided')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            name=name,
+            **args
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+
+class UserAccount(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects=UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRE_FIELDS = ['name']
 
 # Create your models here.
