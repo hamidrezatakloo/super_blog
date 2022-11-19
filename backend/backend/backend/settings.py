fb68110b2addf0c3714c209c9d2451292efa600c
hamidreza <takloohamidreza@gmail.com>
2022-11-19 02:52:29 -0500

    Fix: Config email settings

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.UserAccount'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'superblogautomail@gmail.com'
+EMAIL_HOST_PASSWORD = 'uutfbsqlpmysqbjx'
