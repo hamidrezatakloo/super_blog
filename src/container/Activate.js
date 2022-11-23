2895c554bcbb356587b972ffad2c9425e4030c77
hamidreza <takloohamidreza@gmail.com>
2022-11-23 06:55:00 -0500

    Fix: send {uid,token} to backend for activation

--- src/container/Activate.js
+++ src/container/Activate.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+//Post {uid} and {token} to active account
 const Activate = () => {
-  return <div>your account activated successfully </div>;
+  const [message, setMessage] = useState("");
+  let params = useParams();
+  useEffect(() => {
+    axios
+      .post("http://127.0.0.1:8000/auth/users/activation/", params)
+      .then((response) =>
+        console.log(setMessage("your account activated successfully"))
+      )
+      .catch((err) => setMessage("invalid token"));
+  }, []);
+  return <div>{message}</div>;
 };
 
 export default Activate;
