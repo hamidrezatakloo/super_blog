e347a9d4dfcda35fc923047d257c5f991aabaf07
hamidreza <takloohamidreza@gmail.com>
2022-11-23 08:04:10 -0500

    Fix: Add message with proper color in a box

--- src/container/Activate.js
+++ src/container/Activate.js
@@ -18,7 +18,19 @@ const Activate = () => {
         setMessage("expired token");
       });
   }, []);
-  return <div>{message}</div>;
+  return (
+    <div className="w-10/12 mx-auto flex-1 flex items-center justify-center">
+      <p
+        className={` ${
+          status
+            ? "border-green-400 text-green-400"
+            : "border-red-400 text-red-400"
+        }  shadow border-t-8  h-[200px] w-full text-center text-4xl capitalize flex justify-center items-center`}
+      >
+        {message}
+      </p>
+    </div>
+  );
 };
 
 export default Activate;
