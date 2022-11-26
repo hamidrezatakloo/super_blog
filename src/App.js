da101804478a7d3868134877313cc9eb95424df2
hamidreza <takloohamidreza@gmail.com>
2022-11-26 11:23:12 -0500

    Fix: Redirect user if logged in or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./container/Home";
 import Blog from "./container/Blog";
 import Layout from "./hoc/Layout";
@@ -7,7 +7,9 @@ import Login from "./container/Login";
 import Activate from "./container/Activate";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./container/Dashboard";
+import { useSelector } from "react-redux";
 function App() {
+  const token = useSelector((state) => state.authentication.token);
   return (
     <BrowserRouter>
       <Layout>
@@ -15,10 +17,19 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/blog" element={<Blog />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
+          <Route
+            path="/login"
+            element={token ? <Navigate to="/dashboard" /> : <Login />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={token ? <Navigate to="dashboard" /> : <Signup />}
+          ></Route>
           <Route path="/activate/:uid/:token" element={<Activate />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="/dashboard"
+            element={token ? <Dashboard /> : <Navigate to="/login" />}
+          ></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
