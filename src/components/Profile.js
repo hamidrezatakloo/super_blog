dee974a575a5f45f1a3bee9b497fc5efa5e13c91
hamidreza <takloohamidreza@gmail.com>
2022-11-27 08:23:28 -0500

    Fix: Add image and user info html tags

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,17 @@
 const Profile = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <span>Gabriela Richardson</span>
+          <span>
+            email:<span>Richardson@gmail.com</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
