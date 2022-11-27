68831857664db6f956d26ff72b4c60f3cb32c630
hamidreza <takloohamidreza@gmail.com>
2022-11-27 17:26:32 -0500

    Fix: Correct path of image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ const Profile = () => {
   return (
     <div className="col-span-10 p-10">
       <div className="flex">
-        <img src="./profile.jpg" alt="profile" className="w-[350px] rounded" />
+        <img src="/profile.jpg" alt="profile" className="w-[350px] rounded" />
         <div className="flex flex-col p-6 gap-4">
           <span className="text-4xl font-medium text-slate-800">
             Gabriela Richardson
