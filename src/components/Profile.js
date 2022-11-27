9b32a375fee33aa66dc53edc7c14093c397319db
hamidreza <takloohamidreza@gmail.com>
2022-11-27 09:22:33 -0500

    Fix: Profile span 10 col of dashboard

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 const Profile = () => {
   return (
-    <div>
+    <div className="col-span-10">
       <div className="flex">
         <img src="./profile.jpg" alt="profile" className="w-[350px] rounded" />
         <div className="flex flex-col p-6 gap-4">
