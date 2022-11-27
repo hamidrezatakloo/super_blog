b8460a403b43cbda3fa65428f3c9c9340fff1fa4
hamidreza <takloohamidreza@gmail.com>
2022-11-27 10:54:45 -0500

    Fix: Remove padding and margins and set full width

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 import Profile from "../components/Profile";
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-12 w-11/12 mx-auto py-8">
+    <div className="grid grid-cols-12">
       <Sidebar />
       <Profile />
     </div>
