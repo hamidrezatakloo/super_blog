c7f87b7b81309537b70df75b2e80e3d46fd742cc
hamidreza <takloohamidreza@gmail.com>
2022-11-27 09:21:22 -0500

    Fix: Set grid display with 12 col

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -1,5 +1,12 @@
+import Sidebar from "../components/Sidebar";
+import Profile from "../components/Profile";
 const Dashboard = () => {
-  return <div>HELLO IAM DASHBOARD</div>;
+  return (
+    <div className="grid grid-cols-12 w-11/12 mx-auto">
+      <Sidebar />
+      <Profile />
+    </div>
+  );
 };
 
 export default Dashboard;
