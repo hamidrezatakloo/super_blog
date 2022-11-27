8b5094e41dca7c796d32191124f86f1adf952111
hamidreza <takloohamidreza@gmail.com>
2022-11-27 11:35:30 -0500

    Fix: Set height of dashboard to full

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 import Profile from "../components/Profile";
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-12">
+    <div className="grid grid-cols-12 h-full">
       <Sidebar />
       <Profile />
     </div>
