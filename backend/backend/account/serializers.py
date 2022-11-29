97e495c88dfeb24f06d62e98e3b508f1e43e0a0a
hamidreza <takloohamidreza@gmail.com>
2022-11-29 18:04:34 -0500

    Fix: Add custom Serializer for user
    Now when send request to 'users/me' we receive all fields
    (also null and blank fields)

--- backend/backend/account/serializers.py
+++ backend/backend/account/serializers.py
@@ -1,8 +1,13 @@
-from djoser.serializers import UserCreateSerializer
+from djoser.serializers import UserCreateSerializer,UserSerializer
 from django.contrib.auth import get_user_model
 user = get_user_model()
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = user
-        fields = ['id','email','name','password']                 
+        fields = ['id','email','first_name','last_name','password']                 
+
+class CustomUserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        model = user
+        fields = ['id','email','first_name','last_name','password','phone_number','about','date_of_birth','avatar'] 
--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'corsheaders',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -151,7 +154,8 @@ DJOSER = {
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS':{
         'user_create':'account.serializers.CustomUserCreateSerializer',
-        'user':'account.serializers.CustomUserCreateSerializer',
+        'user':'account.serializers.CustomUserSerializer',
+        'current_user':'account.serializers.CustomUserSerializer',
         'user_delete':'account.serializers.CustomUserCreateSerializer'
     }
 }
