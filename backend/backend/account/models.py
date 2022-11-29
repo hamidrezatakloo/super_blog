28b4b400355b9bdc7cb1c9fea151d9f801262590
hamidreza <takloohamidreza@gmail.com>
2022-11-29 16:48:51 -0500

    Fix: Add extra field to user model

--- backend/backend/account/models.py
+++ backend/backend/account/models.py
@@ -4,25 +4,30 @@ from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,Permissi
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,name,password=None):
+    def create_user(self,email,first_name,last_name,password=None):
         if email is None:
             raise ValueError('No email Provided')
 
         email = self.normalize_email(email)
-        user = self.model(email= email,name=name)
+        user = self.model(email= email,first_name=first_name,last_name=last_name)
         user.set_password(password)
         user.save()
         return user
 
 class UserAccount(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=16,blank=True)
+    about = models.CharField(max_length=200,blank=True)
+    date_of_birth = models.DateField(blank=True,null=True)
+    avatar = models.ImageField(upload_to='profile-pics',blank=True,null=True)
 
     objects=UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['first_name','last_name']
 
 # Create your models here.
