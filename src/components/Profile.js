ccd3c68b81e912bec0a89d58170f10f2ee86ab2c
hamidreza <takloohamidreza@gmail.com>
2022-11-30 09:34:22 -0500

    Fix: Set fields to show user data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,17 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
+  const user = useSelector((state) => state.authentication.user);
+  console.log(user);
+
   return (
     <div className="col-span-10 p-10">
       <div className="flex">
-        <img src="/profile.jpg" alt="profile" className="w-[350px] rounded" />
+        <img
+          src={user.avatar ? user.avatar : "/profile.png"}
+          alt="profile"
+          className="w-[350px] rounded"
+        />
         <div className="flex flex-col p-6 gap-4">
           <span className="text-4xl font-medium text-slate-800">
             Gabriela Richardson
@@ -16,23 +25,25 @@ const Profile = () => {
       <div className="flex flex-col lg:flex-row gap-y-4 flex-wrap  my-8">
         <span className="text-slate-500 text-lg w-6/12">
           firstName:&nbsp;
-          <span className="text-slate-800 text-md"></span>
+          <span className="text-slate-800 text-md">{user.first_name}</span>
         </span>
         <span className="text-slate-500 text-lg w-6/12">
           LastName:&nbsp;
-          <span className="text-slate-800 text-md"></span>
+          <span className="text-slate-800 text-md">{user.last_name}</span>
         </span>
         <span className="text-slate-500 text-lg w-6/12">
           PhoneNumber:&nbsp;
-          <span className="text-slate-800 text-md"></span>
+          <span className="text-slate-800 text-md">{user.Phone_number}</span>
         </span>
         <span className="text-slate-500 text-lg w-6/12">
           DateOfBirth:&nbsp;
-          <span className="text-slate-800 text-md"></span>
+          <span className="text-slate-800 text-md">
+            {user.date_of_birth ? user.date_of_birth : ""}
+          </span>
         </span>
         <span className="text-slate-500 text-lg">
           About:&nbsp;
-          <span className="text-slate-800 text-md"></span>
+          <span className="text-slate-800 text-md">{user.about}</span>
         </span>
       </div>
     </div>
