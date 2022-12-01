1a2a60a547a757a79d20a50850305544e9d5a555
hamidreza <takloohamidreza@gmail.com>
2022-12-01 09:18:08 -0500

    Feat: Register and validate inputs with form-hook

--- src/components/Editprofile.js
+++ src/components/Editprofile.js
@@ -1,6 +1,19 @@
+import { useForm } from "react-hook-form";
 const EditProfile = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
-    <form className="col-span-10 flex flex-col gap-16 p-10">
+    <form
+      className="col-span-10 flex flex-col gap-16 p-10"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex flex-col md:flex-row items-center gap-10">
         <img src="/profile.png" accept="image/*" className="w-[200px]" />
         <label
@@ -9,7 +22,13 @@ const EditProfile = () => {
         >
           Change Profile image
         </label>
-        <input id="avatar" name="avatar" type="file" className="hidden" />
+        <input
+          {...register("avatar")}
+          id="avatar"
+          name="avatar"
+          type="file"
+          className="hidden"
+        />
       </div>
       <div className="flex flex-col md:flex-row flex-wrap gap-4 items-center">
         <div className="w-8/12 md:w-4/12">
@@ -21,6 +40,13 @@ const EditProfile = () => {
           </label>
 
           <input
+            {...register("first_name", {
+              required: "This field should not be empty",
+              maxLength: {
+                value: 150,
+                message: "too long!",
+              },
+            })}
             type="text"
             id="first_name"
             name="first_name"
@@ -37,6 +63,13 @@ const EditProfile = () => {
           </label>
 
           <input
+            {...register("last_name", {
+              required: "This field should not be empty",
+              maxLength: {
+                value: 150,
+                message: "too long!",
+              },
+            })}
             type="text"
             id="last_name"
             name="last_name"
@@ -49,6 +82,12 @@ const EditProfile = () => {
           </label>
 
           <input
+            {...register("about", {
+              maxLength: {
+                value: 200,
+                message: "too long!",
+              },
+            })}
             type="text"
             id="about"
             name="about"
@@ -64,7 +103,13 @@ const EditProfile = () => {
           </label>
 
           <input
-            type="text"
+            {...register("phone_number", {
+              maxLength: {
+                value: 16,
+                message: "too long!",
+              },
+            })}
+            type="tel"
             id="phone_number"
             name="phone_number"
             className="h-8 mt-1 w-full rounded border-2 border-gray-200 shadow-sm sm:text-sm outline-none focus:border-cyan-500"
@@ -80,6 +125,7 @@ const EditProfile = () => {
           </label>
 
           <input
+            {...register("date_of_birth")}
             type="date"
             id="date_of_birth"
             name="date_of_birth"
