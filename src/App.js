63fe93aaeaf99e51d9250bc65b6c89e477560c40
hamidreza <takloohamidreza@gmail.com>
2022-12-03 17:30:34 -0500

    Fix: Verify token with server in first render
    Fix: if token is verify we set user ,token and Verify
    in otherwise we remove them from states and localStorages

--- src/App.js
+++ src/App.js
@@ -8,15 +8,35 @@ import Activate from "./container/Activate";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./container/Dashboard";
 import { useSelector, useDispatch } from "react-redux";
-import { SetToken } from "./slices/AuthenticationSlice";
+import { SetToken, SetUser, SetVerify } from "./slices/AuthenticationSlice";
 import Profile from "./components/Profile";
 import NewPost from "./components/NewPost";
 import EditProfile from "./components/Editprofile";
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
   const dispatch = useDispatch();
-  const token = useSelector((state) => state.authentication.token);
-  if (!token && localStorage.hasOwnProperty("token"))
-    dispatch(SetToken(localStorage.getItem("token")));
+  const verify = useSelector((state) => state.authentication.verify);
+  useEffect(() => {
+    if (localStorage.hasOwnProperty("token")) {
+      const token = localStorage.getItem("token");
+      axios
+        .post("http://127.0.0.1:8000/auth/jwt/verify/", { token: token })
+        .then((response) => {
+          dispatch(SetVerify(true));
+          dispatch(SetToken(token));
+          dispatch(SetUser(JSON.parse(localStorage.getItem("user"))));
+          console.log(response);
+        })
+        .catch((error) => {
+          dispatch(SetVerify(false));
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          console.log(error);
+        });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Layout>
@@ -26,16 +46,16 @@ function App() {
           <Route path="/blog" element={<Blog />}></Route>
           <Route
             path="/login"
-            element={token ? <Navigate to="/dashboard" /> : <Login />}
+            element={verify ? <Navigate to="/dashboard" /> : <Login />}
           ></Route>
           <Route
             path="/signup"
-            element={token ? <Navigate to="dashboard" /> : <Signup />}
+            element={verify ? <Navigate to="dashboard" /> : <Signup />}
           ></Route>
           <Route path="/activate/:uid/:token" element={<Activate />}></Route>
           <Route
             path="/dashboard"
-            element={token ? <Dashboard /> : <Navigate to="/login" />}
+            element={verify ? <Dashboard /> : <Navigate to="/login" />}
           >
             <Route path="profile" element={<Profile />}></Route>
             <Route path="newPost" element={<NewPost />}></Route>
