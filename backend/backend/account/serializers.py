deea323718bfb51bd274b98aeb13e95bff6b7e84
hamidreza <takloohamidreza@gmail.com>
2022-12-03 09:37:31 -0500

    Fix: make dateOfBirth and avatar optional

--- backend/backend/account/serializers.py
+++ backend/backend/account/serializers.py
@@ -1,5 +1,6 @@
 from djoser.serializers import UserCreateSerializer,UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 user = get_user_model()
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -8,6 +9,8 @@ class CustomUserCreateSerializer(UserCreateSerializer):
         fields = ['id','email','first_name','last_name','password']                 
 
 class CustomUserSerializer(UserSerializer):
+    avatar = serializers.ImageField(required=False)
+    date_of_birth = serializers.DateField(required=False)
     class Meta(UserSerializer.Meta):
         model = user
         fields = ['id','email','first_name','last_name','password','phone_number','about','date_of_birth','avatar'] 
