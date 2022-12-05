de09037a56901b6afa545c6dc1d1116bde3fd12d
hamidreza <takloohamidreza@gmail.com>
2022-12-05 16:35:02 -0500

    Fix: Define url for posts

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from post_api import urls as postsUrls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('posts/', include(postsUrls)),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
