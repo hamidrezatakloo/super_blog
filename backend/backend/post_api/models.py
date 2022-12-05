c667d9857cfc8c3d1fc5279948727dac0a48f7f7
hamidreza <takloohamidreza@gmail.com>
2022-12-05 17:12:13 -0500

    Feat: Create Post model
    Note: Each Post have a title ,description,image
    and an author

--- backend/backend/post_api/models.py
+++ backend/backend/post_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='post-pics',blank=True,null=True)
+    create_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
