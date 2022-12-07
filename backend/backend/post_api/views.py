3008cc23d77f225f715cba9471441f8e413e682b
hamidreza <takloohamidreza@gmail.com>
2022-12-07 03:43:20 -0500

    Feat: Add a view for creating new Post object

--- backend/backend/post_api/views.py
+++ backend/backend/post_api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status , permissions
+from .serializers import PostSerializer
+
+class CreatePostApiView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def post(self,request,*args,**kwargs):
+
+        data = {
+            "title":request.data.get("title"),
+            "description":request.data.get("description"),
+            "image":request.data.get("image"),
+            "author":request.user.id
+        }
+
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
 
 # Create your views here.
