a9a3861badef3815551b4e82193d98c9e8976075
hamidreza <takloohamidreza@gmail.com>
2022-12-07 04:18:15 -0500

    Feat: Add view for getting all post objects

--- backend/backend/post_api/views.py
+++ backend/backend/post_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status , permissions
 from .serializers import PostSerializer
+from .models import Post
 
 class CreatePostApiView(APIView):
 
@@ -24,6 +25,13 @@ class CreatePostApiView(APIView):
         
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+class PostLists(APIView):
 
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request,*args,**kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 # Create your views here.
