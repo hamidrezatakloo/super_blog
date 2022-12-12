79286f66fb3fa9bc82c2b2f29fd5e5b476599fb4
hamidreza <takloohamidreza@gmail.com>
2022-12-12 10:06:27 -0500

    Fix: Show appropriate navbar for user and authUser

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
+import AuthNavbar from "../components/AuthNavbar";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 const Layout = ({ children }) => {
   const [showNav, setShowNav] = useState(true);
   let location = useLocation();
-  const token = useSelector((state) => state.authentication.token);
+  const verify = useSelector((state) => state.authentication.verify);
   //monitor current path if they are login or signup
   useEffect(() => {
     if (
@@ -18,8 +19,9 @@ const Layout = ({ children }) => {
   }, [location]);
 
   return (
-    <div className="flex flex-col h-screen">
-      {showNav && <Navbar />}
+    <div className="flex flex-col min-h-screen relative">
+      {showNav && !verify && <Navbar />}
+      {showNav && verify && <AuthNavbar />}
       {children}
     </div>
   );
