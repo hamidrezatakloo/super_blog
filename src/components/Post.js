78418f2a96b5cf052d52c3faded409572a377b4d
hamidreza <takloohamidreza@gmail.com>
2022-12-12 07:21:08 -0500

    Refactor: Change styles of Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,23 +3,25 @@ const Post = ({ src, title, desc, col }) => {
     <div
       className={`${
         col
-          ? "md:flex-col col-span-12 md:col-span-4 px-2 py-6 first:pt-0 md:first:pt-2 md:py-2 border-b-2 border-gray-200 last:border-0 md:border-0"
+          ? "flex flex-col col-span-12 md:col-span-4 px-2 py-6 first:pt-0 md:first:pt-2 md:py-2 border-b-2 border-gray-200 last:border-0 md:border-0 my-4"
           : "first:pt-0 py-8  border-b-2 border-gray-200 last:border-0"
-      }  flex gap-2`}
+      }  `}
     >
-      <img
-        src={src}
-        className={`${
-          col
-            ? "object-cover w-[150px] sm:w-[250px] md:w-auto md:h-[200px] xl:h-[250px]"
-            : "w-[300px] object-contain"
-        }  rounded hover:opacity-80 transition-all`}
-      />
-      <div className="flex flex-col justify-start sm:justify-center pl-2 pt-2">
-        <h3 className="font-bold text-sm sm:text-xl mb-2">{title}</h3>
-        <p className="hidden sm:line-clamp-3 text-gray-500 font-medium">
-          {desc}
-        </p>
+      <div className={`${col ? "md:flex-col" : ""} flex-1 flex gap-2`}>
+        <img
+          src={src}
+          className={`${
+            col
+              ? "object-cover w-[150px] sm:w-[250px] md:w-auto md:h-[200px] xl:h-[250px]"
+              : "w-[300px] object-contain"
+          }  rounded hover:opacity-80 transition-all`}
+        />
+        <div className="flex flex-1 flex-col justify-start sm:justify-between pl-2 pt-2">
+          <h3 className="font-bold text-sm sm:text-xl mb-2">{title}</h3>
+          <p className="hidden sm:line-clamp-3 text-gray-500 font-medium">
+            {desc}
+          </p>
+        </div>
       </div>
     </div>
   );
