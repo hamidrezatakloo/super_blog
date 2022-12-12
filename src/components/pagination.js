244b7dad491e1a882cc8c136c394a9a8191f61d5
hamidreza <takloohamidreza@gmail.com>
2022-12-12 08:05:12 -0500

    Fix: Render pagination base on pageNumbers state

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,78 +1,33 @@
-const Pagination = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { SetActivePage } from "../slices/PaginationSlice";
+const PaginateSquare = ({ active, p }) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(SetActivePage(p));
+  };
   return (
-    <ol class="flex justify-center gap-1 text-xs font-medium">
-      <li>
-        <a
-          href="#"
-          class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100"
-        >
-          <span class="sr-only">Prev Page</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </li>
+    <li>
+      <button
+        onClick={handleClick}
+        href="#"
+        className={`${
+          active ? "bg-blue-600 text-white" : ""
+        } block h-8 w-8 rounded border border-gray-100 text-center leading-8`}
+      >
+        {p}
+      </button>
+    </li>
+  );
+};
 
-      <li>
-        <a
-          href="#"
-          class="block h-8 w-8 rounded border border-gray-100 text-center bg-blue-600 text-white leading-8"
-        >
-          1
-        </a>
-      </li>
-
-      <li class="block h-8 w-8 rounded border-blue-600 text-center leading-8">
-        2
-      </li>
-
-      <li>
-        <a
-          href="#"
-          class="block h-8 w-8 rounded border border-gray-100 text-center leading-8"
-        >
-          3
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="#"
-          class="block h-8 w-8 rounded border border-gray-100 text-center leading-8"
-        >
-          4
-        </a>
-      </li>
-
-      <li>
-        <a
-          href="#"
-          class="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100"
-        >
-          <span class="sr-only">Next Page</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </li>
+const Pagination = () => {
+  const pageNumbers = useSelector((state) => state.pagination.pageNumbers);
+  const activePage = useSelector((state) => state.pagination.activePage);
+  return (
+    <ol className="flex flex-wrap justify-center gap-1 text-xs font-medium">
+      {[...Array(pageNumbers)].map((v, i) => (
+        <PaginateSquare key={i} p={i + 1} active={activePage === i + 1} />
+      ))}
     </ol>
   );
 };
