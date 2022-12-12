df0dc2748bf1b59df758b29dbe255844be7d0181
hamidreza <takloohamidreza@gmail.com>
2022-12-12 10:00:33 -0500

    Fix: replace grid with flex in dashboard component

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-12 h-full">
+    <div className="flex-1 flex overflow-hidden">
       <Sidebar />
       <Outlet />
     </div>
