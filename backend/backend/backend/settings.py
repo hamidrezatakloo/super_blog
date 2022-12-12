71cd9efdb6a1c86833b739697360640642c2a281
hamidreza <takloohamidreza@gmail.com>
2022-12-12 10:15:48 -0500

    Fix: Increase lifetime of jwt-token to 1 day

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,6 +175,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 CORS_ALLOWED_ORIGINS = [
