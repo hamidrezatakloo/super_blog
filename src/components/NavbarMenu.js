8d7a80d0fff74bd2bf423ba59e45dfb1b3fbd734
hamidreza <takloohamidreza@gmail.com>
2022-12-12 09:52:42 -0500

    Feat: Add a component for navbar menus

---
+++ src/components/NavbarMenu.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+const NavbarMenu = () => {
+  return (
+    <Fragment>
+      <li className="text-gray-500 transition hover:text-gray-500/75">
+        <Link to="/about">About</Link>
+      </li>
+
+      <li className="text-gray-500 transition hover:text-gray-500/75">
+        <Link to="/blog">Blog</Link>
+      </li>
+    </Fragment>
+  );
+};
+
+export default NavbarMenu;
