43b41abbfb93e3544cd3f010af41587664adcc48
hamidreza <takloohamidreza@gmail.com>
2022-12-12 06:17:02 -0500

    Fix: Add a separate serializer for post lists
    Note: this serializer show fullName attr that of author field(ForeignKey)
    in UserAccount model

--- backend/backend/post_api/serializers.py
+++ backend/backend/post_api/serializers.py
@@ -5,3 +5,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True, slug_field='full_name')
+    class Meta:
+        model = Post
+        fields = "__all__"
