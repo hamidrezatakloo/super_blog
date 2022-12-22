f6477b778e64d319fbb563eb5e353cc7cd72e5dc
hamidreza <takloohamidreza@gmail.com>
2022-12-22 14:11:44 -0500

    Fix: Update password of Email App

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -143,7 +143,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'superblogautomail@gmail.com'
-EMAIL_HOST_PASSWORD = 'uutfbsqlpmysqbjx'
+EMAIL_HOST_PASSWORD = 'fhoarooxaqhqwbkr'
 
 DOMAIN = 'localhost:3000'
 
