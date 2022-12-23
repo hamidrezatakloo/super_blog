04e9438a1bef7a80e15652be9665ce32c4e64b16
hamidreza <takloohamidreza@gmail.com>
2022-12-23 07:34:31 -0500

    Fix: Allow cors for all origins

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -178,7 +178,4 @@ SIMPLE_JWT = {
    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+CORS_ALLOW_ALL_ORIGINS = True
